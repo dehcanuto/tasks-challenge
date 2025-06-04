c6003b587c8df6c06589c3b5960f698f3a3e2fac
André Canuto <andrecanutoc@gmail.com>
2025-06-04 15:25:23 -0300

    Feat: nestjs version done

--- nestjs-version/src/tasks/tasks.service.ts
+++ nestjs-version/src/tasks/tasks.service.ts
@@ -1,11 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Task, TaskDocument } from './schemas/task.schema';
 import { Model } from 'mongoose';
+import { Task, TaskDocument } from './schemas/task.schema';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TasksService {
   constructor(@InjectModel(Task.name) private taskModel: Model<TaskDocument>) {}
 
-  
+  async create(createTaskDto: CreateTaskDto): Promise<Task> {
+    const createdTask = new this.taskModel(createTaskDto);
+    return createdTask.save();
+  }
+
+  async findAll(): Promise<Task[]> {
+    return this.taskModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Task> {
+    const task = await this.taskModel.findById(id).exec();
+    if (!task) throw new NotFoundException('Task not found');
+    return task;
+  }
+
+  async update(id: string, updateTaskDto: UpdateTaskDto): Promise<Task> {
+    const updated = await this.taskModel
+      .findByIdAndUpdate(id, updateTaskDto, { new: true })
+      .exec();
+    if (!updated) throw new NotFoundException('Task not found');
+    return updated;
+  }
+
+  async remove(id: string): Promise<void> {
+    const result = await this.taskModel.findByIdAndDelete(id).exec();
+    if (!result) throw new NotFoundException('Task not found');
+  }
 }
